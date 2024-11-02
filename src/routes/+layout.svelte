1add77ea083f65cf248bd46361e669e523d8d9e7
Noe TATOUD <noe.tatoud@gmail.com>
2024-11-02 18:14:13 +0100

    feat(header): Create header with layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import '../app.css';
 	import Footer from '../components/Footer.svelte';
+	import Header from '../components/Header.svelte';
 	let { children } = $props();
 </script>
 
+<Header />
 {@render children()}
 <Footer />
