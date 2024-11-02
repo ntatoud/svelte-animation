5dd349c6c68b1437f74e4e5248f71e1780b83c1e
Noe TATOUD <noe.tatoud@gmail.com>
2024-11-02 19:39:58 +0100

    feat(hero): Add hero image file

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,9 @@ const config = {
 
 	kit: {
 		alias: {
-			'#components/*': './src/components/*'
+			'#components/*': './src/components/*',
+			'#lib/*': './src/lib/*',
+			'#assets/*': './src/lib/assets/*'
 		},
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
