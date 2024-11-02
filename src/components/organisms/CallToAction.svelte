b876f3f7a24cee01a62487f09758f60e80d8631a
Noe TATOUD <noe.tatoud@gmail.com>
2024-11-03 00:03:00 +0100

    feat(cta): Create CallToAction component

---
+++ src/components/organisms/CallToAction.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Bounded from '#components/atoms/Bounded.svelte';
+	import LinkButton from '#components/atoms/LinkButton.svelte';
+	import PlainLogo from '#components/atoms/PlainLogo.svelte';
+</script>
+
+<Bounded className="relative py-32 text-center font-medium md:py-40">
+	<div
+		class="glow absolute -z-10 aspect-square w-full max-w-sm rounded-full bg-violet-500/50 blur-[160px] filter"
+	></div>
+	<div
+		class="glass-container rounded-lg bg-gradient-to-b from-gray-800 to-gray-900 p-4 md:rounded-xl"
+	>
+		<PlainLogo />
+	</div>
+	<h2 class="mt-8 max-w-xl text-balance text-5xl">AI powered insights for videographers</h2>
+	<LinkButton href="/" className="mt-6">Start now</LinkButton>
+</Bounded>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,11 @@
 	import Hero from '#components/organisms/Hero.svelte';
 	import Showcase from '#components/organisms/Showcase.svelte';
 	import Integrations from '#components/organisms/Integrations.svelte';
+	import CallToAction from '#components/organisms/CallToAction.svelte';
 </script>
 
 <Hero />
 <Bento />
 <Showcase />
 <Integrations />
+<CallToAction />
