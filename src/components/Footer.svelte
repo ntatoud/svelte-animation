fad223a876ffee38d065af229876bd158bbf6f8f
Noe TATOUD <noe.tatoud@gmail.com>
2024-11-02 17:23:27 +0100

    feat(footer): Create footer with layout

---
+++ src/components/Footer.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	const footerItem = [
+		{ label: 'Github', href: 'https://github.com/ntatoud' },
+		{ label: 'LinkedIn', href: 'https://www.linkedin.com/in/noetatoud' }
+	];
+</script>
+
+<footer>
+	<nav
+		class="flex flex-col items-center justify-between gap-6 border-t border-gray-600 px-8 py-7 md:flex-row"
+		aria-label="Footer"
+	>
+		<a href="/">
+			<!-- TODO: Add LOGO -->
+			LOGO
+		</a>
+		<ul class="flex gap-6">
+			{#each footerItem as item (item.label)}
+				<li>
+					<a href={item.href} class="inline-flex min-h-11 items-center">
+						{item.label}
+					</a>
+				</li>
+			{/each}
+		</ul>
+	</nav>
+</footer>
